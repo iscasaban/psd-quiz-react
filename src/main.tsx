9f9a43c47b6d65cf983d53ca4634e62a55e0ac5a
iscasaban <93005157+iscasaban@users.noreply.github.com>
2026-01-16 13:00:21 +0100

    Update theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme/theme.ts";
 import { CssBaseline } from "@mui/material";
-import "@fontsource/rubik/400.css";
-import "@fontsource/rubik/500.css";
-import "@fontsource/rubik/600.css";
+import "@fontsource/nunito/500.css";
+import "@fontsource/nunito/700.css";
+import "@fontsource/nunito/800.css";
 import { QuestionProvider } from "./context/QuestionContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
